b5e0b4d378dd816d35a176f03575766f704f0fba
Z4ffarani <zaffarani.kaique@gmail.com>
2024-05-20 16:54:56 -0300

    branch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+function janela(){
+    let categoria = document.getElementById("Categoria")
+    if (categoria.value === "janela_1") {
+        
+        }
+}
+
+function criarPost() {
+    let descricao = document.getElementById("digiPost").value
+    let texto = document.getElementById("nomePost")
+    let imgPost_1 = document.getElementById("imgPost_1")
+    let escolhadefoto = document.getElementById("insertImg_1").value
+
+    texto.innerText = descricao
+    imgPost_1.src = escolhadefoto
+}
+
